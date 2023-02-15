858eac6a9144c00e249fe417ddfaa03d364e8d73
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-02-15 19:12:25 +0000

    build: update tailwind conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      sm: "600px",
+      md: "906px",
+      lg: "1280px",
+      xl: "1440px",
+    },
     extend: {
       fontFamily: {
         sans: ["Nunito Sans", ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase }) {
+      addBase({
+        html: { fontSize: "16px" },
+      });
+    }),
+    require("@tailwindcss/forms"),
+  ],
 };
