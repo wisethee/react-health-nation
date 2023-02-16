4ff113e51bbd393dfbc2b23aba92ff02fbfccda8
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-02-16 21:27:45 +0000

    feat: update header

--- src/app/core/layout/header/header.jsx
+++ src/app/core/layout/header/header.jsx
@@ -1,17 +1,19 @@
 import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
 import { signOutAuthUser } from "../../utils/firebase/auth.firebase";
-
 import { UserContext } from "../../contexts/user";
 
-import logo from "../../../../assets/logo.svg";
 import Button from "../../components/button/button";
+import { ReactComponent as Logo } from "../../../../assets/logo.svg";
 
 const Header = () => {
   const { currentUser } = useContext(UserContext);
-
   const navigate = useNavigate();
+
+  const signOutAuthUserHandler = async () => {
+    await signOutAuthUser();
+    navigate("/");
+  };
 
   return (
     <header className="">
@@ -20,13 +22,16 @@ const Header = () => {
           <div className="">
             <Link className="" to="/">
               <span className="sr-only">Health Nation</span>
-              <img className="" src={logo} alt="Logo" />
+              <Logo className="" />
             </Link>
           </div>
 
           {currentUser ? (
             <div className="">
-              <Button buttonType={"transparent"} onClick={signOutAuthUser}>
+              <Button
+                buttonType={"transparent"}
+                onClick={signOutAuthUserHandler}
+              >
                 Sign out
                 <span className="pl-2" aria-hidden="true">
                   &rarr;
