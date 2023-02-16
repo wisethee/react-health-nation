e29f38d4fd6aff8694a86ca13cfc3224d0912e98
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-02-16 21:34:20 +0000

    feat: update main component styles

--- src/app/core/layout/main/main.jsx
+++ src/app/core/layout/main/main.jsx
@@ -2,8 +2,14 @@ import { Outlet } from "react-router-dom";
 
 const Main = () => {
   return (
-    <main id="main" className="main" role="main">
-      <Outlet />
+    <main
+      id="main"
+      className="main flex flex-col px-4 md:px-8 lg:px-12 xl:px-16"
+      role="main"
+    >
+      <div className="max-w-[1280px] mx-auto w-full">
+        <Outlet />
+      </div>
     </main>
   );
 };
