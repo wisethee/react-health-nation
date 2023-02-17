15dfbe4fb0608b5f7fc2e22617816e2193d5da88
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-02-17 19:48:37 +0000

    refactor: remove header component

--- src/app/routes/home/home.jsx
+++ src/app/routes/home/home.jsx
@@ -1,16 +1,16 @@
-import { Fragment } from 'react';
-import Hero from '../../core/components/hero/hero';
-import Section from '../../core/components/section/section';
-import circle1 from '../../../assets/circle-orange.svg';
-import circle2 from '../../../assets/circle-purple.svg';
+import { Fragment } from "react";
+import Section from "../../core/components/section/section";
+import circle1 from "../../../assets/circle-orange.svg";
 
 const Home = () => {
   return (
     <Fragment>
-      <Hero />
-      <Section img={circle1} order={'order-last'} justify={'justify-start'} btnColor={'orange'} />
-      <Section img={circle2} order={'order-first'} justify={'justify-end'} btnColor={'grey'} />
-      <Section img={circle1} order={'order-last'} justify={'justify-start'} btnColor={'tertiary'} />
+      <Section
+        img={circle1}
+        order={"order-first"}
+        justify={"justify-start"}
+        btnColor={"orange"}
+      />
     </Fragment>
   );
 };
