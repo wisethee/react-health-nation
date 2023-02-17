64a00e329fc46daadce0b63cbb0d53ba63100dab
Matthew Osborne <momatthewosborne1@gmail.com>
2023-02-17 18:11:28 +0000

    css

--- src/app/core/layout/header/header.jsx
+++ src/app/core/layout/header/header.jsx
@@ -1,20 +1,20 @@
 import { Link } from 'react-router-dom';
 import logo from '../../../../assets/logo.svg';
 import Button from '../../components/button/button';
-
+import './header.css'
 const Header = () => {
   return (
     <header className=''>
-      <div className='container mx-auto'>
+      <div className='container mx-auto '>
         <nav className='' aria-label='Global'>
-          <div className=''>
+          <div className='header-logo'>
             <Link className='' to='/'>
               <span className='sr-only'>Health Nation</span>
               <img className='' src={logo} alt='Logo' />
             </Link>
           </div>
 
-          <div className=''>
+          <div className='button-div'>
             <Button buttonType={'transparent'}>
               Log in
               <span className='pl-2' aria-hidden='true'>
