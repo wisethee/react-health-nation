62f53f25c508414a34d1a60b3053f5c639fd61bb
Matthew Osborne <momatthewosborne1@gmail.com>
2023-02-21 20:13:16 +0000

    built circle

--- src/app/core/components/animate/animate.jsx
+++ src/app/core/components/animate/animate.jsx
@@ -1,8 +1,26 @@
 import {gsap} from "gsap";
 import { useLayoutEffect, useRef } from "react";
-
+import "./animate.css";
 const Animate = () => {
-    console.log("hello")
+    const app = useRef()
+  useLayoutEffect(()=> {
+    let ctx = gsap.context(() => {
+      // use scoped selectors
+      gsap.to(".container", { opacity: 0.6 });
+      gsap.to(".circle", { duration: 2, x:500, repeat: 2, yoyo: true })
+    }, app);
+    return () => ctx.revert()
+  }, []) 
+  
+
+  return (
+    
+      <div  ref={app}>
+        <div className="container">test test</div>
+        <div className="circle"></div>
+      </div>
+  )
+    
 }
 
 export default Animate;
