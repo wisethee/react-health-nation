166408731e8813518b780bb936a645cbd03d21ff
celiyo <96136050+celiyo@users.noreply.github.com>
2023-02-21 21:15:54 +0000

    Add 404 page

--- src/app/core/components/form-input/form-input.jsx
+++ src/app/core/components/form-input/form-input.jsx
@@ -1,17 +1,10 @@
-const FormInput = ({ label, ...otherProps }) => {
+const FormInput = ({ label, other = "", ...otherProps }) => {
   return (
     <label className="flex flex-col w-full text-on-surface-variant opacity-60">
       <span className="text-gray-700">{label}</span>
       <input
         type="text"
-        className="
-                    mt-0
-                    block
-                    w-full
-                    px-0.5
-                    border-0 border-b-2 border-secondary-container
-                    focus:ring-0 focus:border-secondary
-                  "
+        className={`mt-0 block w-full px-0.5 border-0 border-b-2 border-secondary-container focus:ring-0 focus:border-secondary ${other}`}
         {...otherProps}
       />
     </label>
