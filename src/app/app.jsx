f68fb85ce2f242ecc7f2a0401da4cc6e2c3573fa
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-02-21 21:35:33 +0000

    feat: add data context

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -8,7 +8,6 @@ import Main from "./core/layout/main/main";
 import Auth from "./routes/auth/auth";
 import NotFound from "./routes/not-found/not-found";
 import Home from "./routes/home/home";
-import Experimental from "./routes/experimental/experimental";
 import Donate from "./routes/donate/donate";
 
 const App = () => {
@@ -21,7 +20,6 @@ const App = () => {
           <Route path="*" element={<NotFound />} />
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
-          <Route path="/experimental" element={<Experimental />} />
           <Route path="/donate/:id" element={<Donate />} />
         </Route>
       </Routes>
