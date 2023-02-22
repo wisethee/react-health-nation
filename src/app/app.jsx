018d42d55e8b175c60c6326a6bbe955ae633d205
celiyo <96136050+celiyo@users.noreply.github.com>
2023-02-22 22:00:14 +0000

    Fix animation on the home page

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -9,7 +9,7 @@ import Auth from "./routes/auth/auth";
 import NotFound from "./routes/not-found/not-found";
 import Home from "./routes/home/home";
 import Donate from "./routes/donate/donate";
-import Animate from "./core/components/animate/animate"
+// import Animate from "./core/components/animate/animate"
 import Checkout from "./routes/checkout/checkout";
 
 const App = () => {
@@ -28,7 +28,7 @@ const App = () => {
       </Routes>
 
       <Footer />
-      <Animate />
+      {/* <Animate /> */}
     </Fragment>
   );
 };
